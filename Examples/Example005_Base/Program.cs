c693b6c3021d93685bef557f4ca6dfbabfb52d00
«Areg» <art.artenyan@mail.ru>
2023-10-04 14:01:31 +0300

    Example005 Base

---
+++ Examples/Example005_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 5;
+int b = 1;
+int c = 2;
+int g = 8;
+int r = 6;
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(g > max) max = g;
+if(r > max) max = r;
+
+Console.Write("Max = ");
+Console.Write(max);
